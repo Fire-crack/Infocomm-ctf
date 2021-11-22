53e85a53d9c521051a686f22e0fde585c8d4d88a
Fire-crack <84394443+Fire-crack@users.noreply.github.com>
2021-11-22 23:23:57 +0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# find what flag contains
+# flag = something
+
+if flag == "CTFInfocomm{we_can_do_it}":
+    ciphertext = "yay"
+elif flag == "CTFInfocomm{no_we_can't}":
+    ciphertext = "noo"
+
+# ciphertext = yay
